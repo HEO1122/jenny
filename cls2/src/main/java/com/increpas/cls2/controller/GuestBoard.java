0a733d5e4c7503fc5889c8a6fe1fee13c5b6118f
class02 <class02@Class02T01>
2021-05-18 18:10:51 +0900

    5018 last commit

---
+++ cls2/src/main/java/com/increpas/cls2/controller/GuestBoard.java
@@ -0,0 +1,19 @@
+package com.increpas.cls2.controller;
+
+import org.springframework.stereotype.*;
+
+/**
+ * 	이 클래스는 방명록 관련 요청을 처리할 컨트롤러 클래스
+ * @author	전은석
+ * @since	2021.05.18
+ * @version	v.1.0
+ * @see
+ * 			작업이력 ]
+ * 				2021.05.18	- 전은석
+ * 					작업 내용 : 클래스 생성
+ *
+ */
+@Controller
+public class GuestBoard {
+
+}
---
+++ cls2/src/main/java/com/increpas/cls2/controller/ReBoard.java
@@ -0,0 +1,21 @@
+package com.increpas.cls2.controller;
+
+import org.springframework.stereotype.*;
+
+/**
+ * 	이 클래스는 댓글 게시판 관련 요청 처리 컨트롤러 클래스
+ * @author	전은석
+ * @since	2021.05.18
+ * @version	v.1.0
+ * @see
+ * 			작업이력 ]
+ * 				2021.05.18 
+ * 					- 담당자 	: 전은석
+ * 					- 작업내용 	: 클래스 제작
+ *
+ */
+
+@Controller
+public class ReBoard {
+
+}
