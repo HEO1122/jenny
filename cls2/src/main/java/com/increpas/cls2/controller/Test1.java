36f8a40b3779668e4a27d19292646e9b7b429f1b
class02 <class02@Class02T01>
2021-06-01 10:34:03 +0900

    survey button add commit

--- cls2/src/main/java/com/increpas/cls2/controller/Test1.java
+++ cls2/src/main/java/com/increpas/cls2/controller/Test1.java
@@ -1,15 +1,26 @@
 package com.increpas.cls2.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.increpas.cls2.dao.SurveyDao;
+
 @Controller
 public class Test1 {
+	@Autowired
+	SurveyDao sDao;
 	
 	@RequestMapping("/main.cls")
-	public String getMain() {
-		return "main";
+	public ModelAndView getMain(ModelAndView mv) {
+		
+		int cnt = 0 ;
+		cnt = sDao.getPCount();
+		mv.addObject("SCOUNT", cnt);
+		
+		mv.setViewName("main");
+		return mv;
 	}
 
 	@RequestMapping("/test02/test03.cls")
